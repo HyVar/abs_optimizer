364945a4f6b6d7f0cc3c4e1ffaa64618261a66db
jacopo <jacopom@ifi.uio.no>
2017-06-21 15:03:42 +0200

    paramter changes, update run_smac script

--- run_smac.sh
+++ run_smac.sh
@@ -1,9 +1,47 @@
 #!/bin/bash
 
-# uncomment the following line to check the scenario
-#smac/util/verify-scenario --scenarios smac_scenario/scenario.txt --verify-instances false
+# option --restore to restart
+# option warmup <dir> to use previous exectution to restart
+# dir can be obtained by
+# smac/util/state-merge --directories smac-output/test --scenario-file scenario.txt --outdir <dir>
 
-smac/smac --scenario-file scenario.txt --seed 1 --validation false \
-	--rungroup test \
-	--retry-crashed-count 2
-#  --abort-on-first-run-crash
+export SMAC_MEMORY=2048
+
+PAR_PROC=2
+
+OPT="--scenario-file scenario.txt"
+OPT=$OPT" --initial-incumbent RANDOM"
+OPT=$OPT" --validation false"
+OPT=$OPT" --shared-model-mode true"
+OPT=$OPT" --abort-on-first-run-crash false"
+
+#		--retry-crashed-count 2 \
+
+if [ $# -eq 1 ] && [ $1 = "--restore" ]; then
+	for ((i=1;i<=$PAR_PROC;i++)); do
+		echo "Restoring running" $i " instance of smac with seed " $i
+		echo "State directory"  smac-output/test/state-run$i
+		smac/smac $OPT \
+			--rungroup test-restore \
+			--restore-scenario smac-output/test/state-run$i \
+		  --seed $i >> "log_run_smac"$i".log" &
+	done
+elif [ $# -eq 2 ] && [ $1 = "--warmup" ]; then
+	for ((i=1;i<=$PAR_PROC;i++)); do
+		echo "Running" $i " instance of smac with seed " $i
+		echo "Warmup state directory" $2
+		smac/smac $OPT \
+			--rungroup test-warmup \
+			--warmstart $2 \
+		  --seed $i >> "log_run_smac"$i".log" &
+	done
+else
+	for ((i=1;i<=$PAR_PROC;i++)); do
+		echo "Running" $i " instance of smac with seed " $i 
+		smac/smac $OPT \
+			--rungroup test \
+			--seed $i > "log_run_smac"$i".log" &
+	done
+fi
+
+wait
